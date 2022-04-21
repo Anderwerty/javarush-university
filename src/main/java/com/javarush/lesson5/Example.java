16b5f578d034f3a5b2fd9995c8ba1a913224730b
Andrii Shylin <andrii.shylin@refinitiv.com>
2022-04-21 21:37:23 +0300

    5.2

--- src/main/java/com/javarush/lesson5/Example.java
+++ src/main/java/com/javarush/lesson5/Example.java
@@ -2,6 +2,6 @@ package com.javarush.lesson5;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("lesson5");
+        System.out.println("lesson5.1");
     }
 }
